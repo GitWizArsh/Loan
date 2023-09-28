62089c535839dd7ff2f3690050c0729367e2929e
Arsh Singh <146023109+GitWizArsh@users.noreply.github.com>
2023-09-28 01:33:31 -0400

    Add files via upload

---
+++ LOAN/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace LOAN
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_Salary = new System.Windows.Forms.TextBox();
+            this.txtYears = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbl_Description = new System.Windows.Forms.Label();
+            this.btn_Check = new System.Windows.Forms.Button();
+            this.btn_Clear = new System.Windows.Forms.Button();
+            this.btn_Exit = new System.Windows.Forms.Button();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(121, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Annual Salary:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(121, 177);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Total years at job:";
+            // 
+            // txt_Salary
+            // 
+            this.txt_Salary.Location = new System.Drawing.Point(229, 93);
+            this.txt_Salary.Name = "txt_Salary";
+            this.txt_Salary.Size = new System.Drawing.Size(125, 27);
+            this.txt_Salary.TabIndex = 2;
+            // 
+            // txtYears
+            // 
+            this.txtYears.Location = new System.Drawing.Point(253, 174);
+            this.txtYears.Name = "txtYears";
+            this.txtYears.Size = new System.Drawing.Size(125, 27);
+            this.txtYears.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(121, 266);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Loan description:";
+            // 
+            // lbl_Description
+            // 
+            this.lbl_Description.AutoSize = true;
+            this.lbl_Description.Location = new System.Drawing.Point(269, 266);
+            this.lbl_Description.Name = "lbl_Description";
+            this.lbl_Description.Size = new System.Drawing.Size(0, 20);
+            this.lbl_Description.TabIndex = 5;
+            // 
+            // btn_Check
+            // 
+            this.btn_Check.Location = new System.Drawing.Point(67, 361);
+            this.btn_Check.Name = "btn_Check";
+            this.btn_Check.Size = new System.Drawing.Size(94, 29);
+            this.btn_Check.TabIndex = 6;
+            this.btn_Check.Text = "Check";
+            this.btn_Check.UseVisualStyleBackColor = true;
+            this.btn_Check.Click += new System.EventHandler(this.btn_Check_Click);
+            // 
+            // btn_Clear
+            // 
+            this.btn_Clear.Location = new System.Drawing.Point(200, 361);
+            this.btn_Clear.Name = "btn_Clear";
+            this.btn_Clear.Size = new System.Drawing.Size(94, 29);
+            this.btn_Clear.TabIndex = 7;
+            this.btn_Clear.Text = "Clear";
+            this.btn_Clear.UseVisualStyleBackColor = true;
+            this.btn_Clear.Click += new System.EventHandler(this.btn_Clear_Click);
+            // 
+            // btn_Exit
+            // 
+            this.btn_Exit.Location = new System.Drawing.Point(365, 361);
+            this.btn_Exit.Name = "btn_Exit";
+            this.btn_Exit.Size = new System.Drawing.Size(94, 29);
+            this.btn_Exit.TabIndex = 8;
+            this.btn_Exit.Text = "Exit";
+            this.btn_Exit.UseVisualStyleBackColor = true;
+            this.btn_Exit.Click += new System.EventHandler(this.btn_Exit_Click);
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Location = new System.Drawing.Point(275, 266);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(0, 20);
+            this.lblDescription.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(749, 450);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.btn_Exit);
+            this.Controls.Add(this.btn_Clear);
+            this.Controls.Add(this.btn_Check);
+            this.Controls.Add(this.lbl_Description);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtYears);
+            this.Controls.Add(this.txt_Salary);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Loan Qualifier";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_Salary;
+        private System.Windows.Forms.TextBox txtYears;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbl_Description;
+        private System.Windows.Forms.Button btn_Check;
+        private System.Windows.Forms.Button btn_Clear;
+        private System.Windows.Forms.Button btn_Exit;
+        private System.Windows.Forms.Label lblDescription;
+    }
+}
+
---
+++ LOAN/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LOAN
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Check_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                const decimal MINIMUM_SALARY = 40000m;
+                const decimal MINIMUM_YEARS_ON_JOB = 2;
+
+                decimal Salary;
+                int yearsOnJob;
+
+                Salary=decimal.Parse(txt_Salary.Text);
+                yearsOnJob = int.Parse(txtYears.Text);
+
+                if(Salary>=MINIMUM_SALARY)
+                {
+                    if(yearsOnJob>=MINIMUM_YEARS_ON_JOB)
+                    {
+                        lblDescription.Text = "You are applicable for loan";
+                    }
+                    else
+                    {
+                        lblDescription.Text = "Sorry you are not applicable for loan";
+                    }
+                }
+                else
+                {
+                    lblDescription.Text = "Sorry you are not applicable for loan";
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btn_Clear_Click(object sender, EventArgs e)
+        {
+            txt_Salary.Text = "";
+            txtYears.Text = "";
+            lblDescription.Text = "";
+
+            txt_Salary.Focus();
+        }
+
+        private void btn_Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
